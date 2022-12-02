f103482d89b12a39a19b4cb53f83a8d7a968277b
devarend <devarend@gmail.com>
2022-12-02 22:21:36 +0100

    get and parse bnb asset, return it in response

--- index.ts
+++ index.ts
@@ -11,10 +11,13 @@ const apiKey = ''
 const apiSecret = ''
 const client = new Spot(apiKey, apiSecret, { baseURL: 'https://testnet.binance.vision'})
 
-app.get('/', async (req, res) => {
+app.get('/bnb', async (req, res) => {
     try {
         const response = await client.account()
-        res.json(response.data)
+        const bnbAsset = response.data.balances.find(({asset}) => asset === 'BNB')
+        if (!bnbAsset) return res.json({message: 'Asset not found'})
+        const availableBnb = parseFloat(bnbAsset.free)
+        res.json({bnb: availableBnb})
     } catch (error) {
         res.json({message: error.message})
     }
