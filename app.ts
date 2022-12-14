c706577c503dfa6b887987973a66d13401c407cd
devarend <devarend@gmail.com>
2022-12-14 11:26:18 +0100

    created new endpoints for trades

--- app.ts
+++ app.ts
@@ -13,6 +13,9 @@ const apiSecret = ''
 export const client = new Spot(apiKey, apiSecret, { baseURL: 'https://testnet.binance.vision'})
 
 app.get('/bnb', walletController.getBnbBalance)
+app.get('/trades', walletController.getTrades)
+app.get('/createTrades', walletController.createTrades)
+
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
