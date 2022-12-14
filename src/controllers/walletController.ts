32471554847e932eb74ea8b179cb49d04185424b
devarend <devarend@gmail.com>
2022-12-14 11:27:40 +0100

    controller functionality to create 10 BNB trades and return the amount of trades

--- src/controllers/walletController.ts
+++ src/controllers/walletController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from "express"
 import {client} from "../../app"
 import {signedBnbBalance} from "../oracles/bnbBalance"
+import {signedBnbTrades} from "../oracles/bnbTrades";
 
 export const getBnbBalance = async (req: Request, res: Response) => {
     try {
@@ -12,5 +13,33 @@ export const getBnbBalance = async (req: Request, res: Response) => {
         res.json({data})
     } catch (error) {
         res.json({message: error.message})
+    }
+}
+
+export const getTrades = async (req: Request, res: Response) => {
+    try {
+        const response = await client.myTrades('BNBUSDT')
+        const data = await signedBnbTrades(1, response.data)
+        res.json({data})
+    } catch (error) {
+        res.json({message: error.message})
+    }
+
+}
+
+export const createTrades = async (req: Request, res: Response) => {
+    try {
+        for (let i = 1; i <= 10; i++) {
+            try {
+                await client.newOrder('BNBUSDT', 'BUY', 'MARKET', {
+                    quantity: 1,
+                })
+            } catch {
+                throw new Error('Something went wrong while creating your BNB orders')
+            }
+        }
+        res.status(200).json({message: 'Created BNB orders'})
+        } catch (error) {
+        res.status(503).json({message: error.message})
     }
 }
